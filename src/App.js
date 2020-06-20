166256b9b37884560ed90a4297d0b71877b7a49c
sinuhe Montero <sinuhem23@gmail.com>
2020-06-20 15:47:29 -0400

    added nav footer and home but still blank

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import ProviderSide from './components/Provider';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 import {
   BrowserRouter as Router,
@@ -13,10 +16,14 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <div>
           <Switch>
+            <Route exact path='/' component={Home} />
+
             <Route exact path='/provider' component={ProviderSide} />
           </Switch>
+          <Footer />
         </div>
       </Router>
     );
